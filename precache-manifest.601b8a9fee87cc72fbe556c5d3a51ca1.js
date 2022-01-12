aa0ab3f22efb93563edfd4e7c1e65af7d1adc7ca
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2022-01-12 20:48:17 +0800

    deploy

---
+++ precache-manifest.601b8a9fee87cc72fbe556c5d3a51ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b9d9dc8c08a176d249",
+    "url": "/giraffe-text-website/css/app.443add89.css"
+  },
+  {
+    "revision": "43083b389d6c19d3e7c99ac1faf94c91",
+    "url": "/giraffe-text-website/index.html"
+  },
+  {
+    "revision": "25b9d9dc8c08a176d249",
+    "url": "/giraffe-text-website/js/app.9340b4aa.js"
+  },
+  {
+    "revision": "13abb8e665c150a68b3a",
+    "url": "/giraffe-text-website/js/chunk-vendors.b75e84ab.js"
+  },
+  {
+    "revision": "2a9fa9d70225977492276d4cc89e0acc",
+    "url": "/giraffe-text-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/giraffe-text-website/robots.txt"
+  }
+]);
